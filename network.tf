ede4f9835d74d85eb4199733632dfd4fd09228c3
terryyin1777@gmail.com <terryyin@terrys-mbp-2.mynetworksettings.com>
2023-04-02 23:20:12 -0400

    add subnet

--- network.tf
+++ network.tf
@@ -1,6 +1,13 @@
 resource "google_compute_network" "my-lab-vpc" {
     project = var.project
-    name = var.network_name
+    name = "my-lab-vpc"
     description = "VPC for the lab prject"
     auto_create_subnetworks = false
 } 
+
+resource "google_compute_subnetwork" "my-lab-subnet" {
+    name = "my-lab-subnet"
+    network = google_compute_network.my-lab-vpc.id
+    ip_cidr_range = "10.10.0.0/16"
+    region = var.region
+}
--- variables.tf
+++ variables.tf
@@ -3,11 +3,6 @@ variable "project" {
     default = "my-lab-382405"
 }
 
-variable "network_name" {
-    type = string
-    default = "my-lab-vpc"
-}
-
 variable "credentials_file" {
     type = string
     default = "/Users/terryyin/my-lab/my-lab-382405-471ab9c3f191.json"
